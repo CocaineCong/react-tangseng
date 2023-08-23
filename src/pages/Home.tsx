8277e36bc7f1fb3f61a93ecbe5ee02304c202af4
FanOne <294350394@qq.com>
2023-08-23 09:36:17 +0800

    feat:search page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,19 @@ const onSearch = (value: string) => console.log(value);
 const Home: React.FC = () => {
     return (
         <div className='search-home'>
-            <input className='search-input' placeholder='搜不了一点' name="password" ></input>
+            <div className='search-content'>
+                <div id="logo">
+                    <img src={require('../assets/images/googlelogo1.png')} className="logo" />
+                </div>
+                <div className="searchbar">
+                    <img src={require('../assets/images/search.png')} className="mg" />
+                    <input type="text" id="search" value="Search Google or type a URL" 
+                        // onFocus="if(value=='Search Google or type a URL')value=''" 
+                        // onblur="if(!value)value='Search Google or type a URL'" 
+                        name="keyword" /> 
+                    <img  src={require('../assets/images/microphone.png')} className="mcp" />
+                </div>  
+            </div>
         </div>
     );
 };
