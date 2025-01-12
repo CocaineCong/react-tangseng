1b2d78128b78fc715b6d8afff2c9d2c549c07a08
FanOne <294350394@qq.com>
2025-01-12 18:35:05 +0800

    fix:loading

--- src/components/HomeSider.tsx
+++ src/components/HomeSider.tsx
@@ -1,73 +1,70 @@
-import {Menu, MenuProps} from "antd";
-import {
-    AppstoreTwoTone,
-    MehTwoTone,
-    SmileTwoTone,
-} from "@ant-design/icons";
-import React, {useEffect, useState} from "react";
-import Sider from "antd/es/layout/Sider";
-import {Link, useLocation, useNavigate} from "react-router-dom";
-
+import { Menu, MenuProps } from 'antd';
+import { AppstoreTwoTone, MehTwoTone, SmileTwoTone } from '@ant-design/icons';
+import React, { useEffect } from 'react';
+import Sider from 'antd/es/layout/Sider';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
 function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
 ): MenuItem {
-    return {
-        key,
-        icon,
-        children,
-        label,
-    } as MenuItem;
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
 }
 
 const items: MenuItem[] = [
-    getItem('主页', '/Product/List', <SmileTwoTone />),
-    getItem('关注的店铺', '/ProductBoss/Star', <AppstoreTwoTone />),
-    getItem('开发中', '/Deving', <MehTwoTone />),
+  getItem('主页', '/Product/List', <SmileTwoTone />),
+  getItem('关注的店铺', '/ProductBoss/Star', <AppstoreTwoTone />),
+  getItem('开发中', '/Deving', <MehTwoTone />),
 ];
 
-function HomeSider(){
-    const location = useLocation()
-    const navigateTo = useNavigate()
-    
-    // 一般加个空数组就是为了模仿componentDidMounted
-    useEffect(()=>{
-        let path = location.pathname;
-        let key = path.split('/')[1];
-    }, [location.pathname])
-    
-    const menuClick = (e:{key:string})=>{
-        navigateTo(e.key)
-    }
-    
-    return (
-        <Sider
-            style = {{
-                overflow: 'auto',
-                height: '100vh',
-                position: 'fixed',
-                left: 0,
-                top: 0,
-                bottom: 0,
-                }}
-           theme="light"
-        >
-        <div style={{ height: 32, margin: 16}}> <h1>FanOneMall</h1> </div>
-            <Menu
-                theme="light"
-                mode="inline"
-                selectedKeys={[location.pathname]}
-                onClick={menuClick}
-                items={items}
-            >
-            </Menu>
-        </Sider>
-    )
+function HomeSider() {
+  const location = useLocation();
+  const navigateTo = useNavigate();
+
+  // 一般加个空数组就是为了模仿componentDidMounted
+  useEffect(() => {
+    let path = location.pathname;
+    let key = path.split('/')[1];
+  }, [location.pathname]);
+
+  const menuClick = (e: { key: string }) => {
+    navigateTo(e.key);
+  };
+
+  return (
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+      theme="light"
+    >
+      <div style={{ height: 32, margin: 16 }}>
+        {' '}
+        <h1>FanOneMall</h1>{' '}
+      </div>
+      <Menu
+        theme="light"
+        mode="inline"
+        selectedKeys={[location.pathname]}
+        onClick={menuClick}
+        items={items}
+      ></Menu>
+    </Sider>
+  );
 }
 
 export default HomeSider;
--- src/pages/SearchDetail.tsx
+++ src/pages/SearchDetail.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Divider, List } from 'antd';
-import '../assets/styles/search-detail.scss'
+import { Divider, List, Spin } from 'antd'; // 引入Spin组件
+import '../assets/styles/search-detail.scss';
 import { useSearchParams } from 'react-router-dom';
 import { searchEngineSearch } from '../api/search-engine';
 import { Code } from '../constant';
@@ -9,65 +9,85 @@ type PaginationPosition = 'top' | 'bottom' | 'both';
 type PaginationAlign = 'start' | 'center' | 'end';
 
 const SearchDetail: React.FC = () => {
-    const [position, setPosition] = useState<PaginationPosition>('bottom');
-    const [align, setAlign] = useState<PaginationAlign>('center');
-    const [searchResList, setSearchResList] = useState<API.SearchResultRespDataSearchList[]>();
-    const [query, setQuery] = useState<any>();
-    const [params] = useSearchParams()
-    let param = params.get('query')
+  const [position, setPosition] = useState<PaginationPosition>('bottom');
+  const [align, setAlign] = useState<PaginationAlign>('center');
+  const [searchResList, setSearchResList] =
+    useState<API.SearchResultRespDataSearchList[]>();
+  const [query, setQuery] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false); // 新增loading状态
+  const [params] = useSearchParams();
+  let param = params.get('query');
 
-    const tangsengSearch = async()=> {
-        const res:any = await searchEngineSearch({query:param})
-        setQuery(param)
-        if (res?.status === Code.SuccessCode) {
-            var resList = res?.data?.search_engine_info_list
-            setSearchResList(resList)
-        }
+  const tangsengSearch = async () => {
+    setLoading(true); // 开始加载数据时，设置loading为true
+    const res: any = await searchEngineSearch({ query: param });
+    setQuery(param);
+    setLoading(false); // 数据加载完成后，设置loading为false
+    if (res?.status === Code.SuccessCode) {
+      const resList = res?.data?.search_engine_info_list;
+      setSearchResList(resList);
     }
+  };
 
-    useEffect(()=>{
-        tangsengSearch()
-    },[])
+  useEffect(() => {
+    tangsengSearch();
+  }, []);
 
-    const handleKeyDown = async (event:any) => {
-        if (event.keyCode === Code.KEY_CODE_DOWN) {
-            const res:any = await searchEngineSearch({query:event?.target?.value})
-            setQuery(event?.target?.value)
-            if (res?.status === Code.SuccessCode) {
-                var resList = res?.data?.search_engine_info_list
-                setSearchResList(resList)
-            }
-        }
+  const handleKeyDown = async (event: any) => {
+    if (event.keyCode === Code.KEY_CODE_DOWN) {
+      setLoading(true); // 搜索时设置loading为true
+      const res: any = await searchEngineSearch({
+        query: event?.target?.value,
+      });
+      setQuery(event?.target?.value);
+      setLoading(false); // 搜索完成后，设置loading为false
+      if (res?.status === Code.SuccessCode) {
+        const resList = res?.data?.search_engine_info_list;
+        setSearchResList(resList);
+      }
     }
+  };
 
-    return (
-        <div className='search-detail-contain'>
-            <div className='search-detail-header'>
-                <div className='search-home-compose'>
-                    <div className="searchbar-compose">
-                        <img src={require('../assets/images/search.png')} className="mg" />
-                        <input 
-                            type="text" 
-                            id="search" 
-                            placeholder="Search Tangseng or type a URL" 
-                            name="keyword" 
-                            defaultValue={query}
-                            onKeyDown={handleKeyDown}
-                        /> 
-                        <img src={require('../assets/images/microphone.png')} className="mcp" />
-                    </div>
-                </div>
-            </div>
-            <Divider />
-            <List
-                className='search-detail-info'
-                pagination={{ position, align }}
-                size="large"
-                dataSource={searchResList}
-                renderItem={(item) => <List.Item>{item?.desc}</List.Item>}
+  return (
+    <div className="search-detail-contain">
+      <div className="search-detail-header">
+        <div className="search-home-compose">
+          <div className="searchbar-compose">
+            <img src={require('../assets/images/search.png')} className="mg" />
+            <input
+              type="text"
+              id="search"
+              placeholder="Search Tangseng or type a URL"
+              name="keyword"
+              defaultValue={query}
+              onKeyDown={handleKeyDown}
             />
+            <img
+              src={require('../assets/images/microphone.png')}
+              className="mcp"
+            />
+          </div>
         </div>
-    )
+      </div>
+      <Divider />
+
+      {/* 在加载数据时显示 Spin 动画 */}
+      {loading ? (
+        <div className="loading-container">
+          tangseng searching...
+          <Spin size="large" />
+        </div>
+      ) : (
+        <List
+          className="search-detail-info"
+          pagination={{ position, align }}
+          size="large"
+          dataSource={searchResList}
+          renderItem={(item) => <List.Item>{item?.desc}</List.Item>}
+        />
+      )}
+    </div>
+  );
 };
 
 export default SearchDetail;
